dd2e6b68ebe6c39912391c82a1d3b917f6972953
mehme <mehme@DESKTOP-73I52K7>
2023-07-02 17:36:56 +0300

    Add project files.

---
+++ E-TicaretSitesiMVC/Models/Siniflar/Cari.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace E_TicaretSitesiMVC.Models.Siniflar
+{
+    public class Cari
+    {
+        [Key]
+        public int CariID { get; set; }
+        public string CariAd { get; set; }
+        public string CariSoyad { get; set; }
+        public string CariSehir { get; set; }
+        public string CariMail { get; set; }
+    }
+}
