e923b6a6e1c4dde223544e152d863c271723f0e4
mabattal <mehmetalibattal@gmail.com>
2023-07-04 21:38:13 +0300

    Departman Index

---
+++ E-TicaretSitesiMVC/Controllers/DepartmanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_TicaretSitesiMVC.Models.Siniflar;
+
+namespace E_TicaretSitesiMVC.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Departmans.ToList();
+
+            return View(degerler);
+        }
+    }
+}
