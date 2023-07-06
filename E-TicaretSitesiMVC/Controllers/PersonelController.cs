bb2e2d0fc76c6839c4a865675d2e40181963969c
mabattal <mehmetalibattal@gmail.com>
2023-07-06 21:06:56 +0300

    Personel ekleme

--- E-TicaretSitesiMVC/Controllers/PersonelController.cs
+++ E-TicaretSitesiMVC/Controllers/PersonelController.cs
@@ -16,5 +16,39 @@ namespace E_TicaretSitesiMVC.Controllers
             var deger = context.Personels.Where(x => x.Sil == false).ToList();
             return View(deger);
         }
+
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            Personel personel = new Personel();
+
+            // Dropdown için seçenekleri oluşturuyoruz
+            List<SelectListItem> dpt = (from x in context.Departmans.ToList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.DepartmanAd,
+                                                    Value = x.DepartmanID.ToString()
+                                                }).ToList();
+            // ViewBag üzerinden seçenekleri View'a gönderiyoruz
+            ViewBag.Departmanlar = dpt;
+
+            List<SelectListItem> durumlar = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Aktif", Value = "true" },
+                new SelectListItem { Text = "Pasif", Value = "false" }
+            };
+
+            ViewBag.Durumlar = durumlar;
+
+            return View(personel);
+        }
+
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel personel)
+        {
+            context.Personels.Add(personel);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
