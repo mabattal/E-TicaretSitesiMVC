7ee5a50e3c744ad6ef0a2ba05ca181e6056e2435
mabattal <mehmetalibattal@gmail.com>
2023-07-06 20:18:43 +0300

    Personel Controller ve Index

---
+++ E-TicaretSitesiMVC/Controllers/PersonelController.cs
@@ -0,0 +1,20 @@
+﻿using E_TicaretSitesiMVC.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_TicaretSitesiMVC.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var deger = context.Personels.Where(x => x.Sil == false).ToList();
+            return View(deger);
+        }
+    }
+}
