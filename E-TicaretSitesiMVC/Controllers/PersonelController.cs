ed07df73b5de54421485aec0375349898e14967a
mabattal <mehmetalibattal@gmail.com>
2023-07-06 21:39:21 +0300

    Personel Sil, Güncelle, Satış Detay

--- E-TicaretSitesiMVC/Controllers/PersonelController.cs
+++ E-TicaretSitesiMVC/Controllers/PersonelController.cs
@@ -50,5 +50,48 @@ namespace E_TicaretSitesiMVC.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult PersonelGetir(int id)
+        {
+            var prs = context.Personels.Find(id);
+
+            // Dropdown için seçenekleri oluşturuyoruz
+            List<SelectListItem> dpt = (from x in context.Departmans.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.DepartmanAd,
+                                            Value = x.DepartmanID.ToString()
+                                        }).ToList();
+            // ViewBag üzerinden seçenekleri View'a gönderiyoruz
+            ViewBag.Departmanlar = dpt;
+
+            List<SelectListItem> durumlar = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Aktif", Value = "true" },
+                new SelectListItem { Text = "Pasif", Value = "false" }
+            };
+
+            ViewBag.Durumlar = durumlar;
+            return View("PersonelEkle", prs);
+        }
+
+        public ActionResult PersonelSil(int id)
+        {
+            var p = context.Personels.Find(id);
+            p.Sil = true;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelGuncelle(Personel personel)
+        {
+            var p = context.Personels.Find(personel.PersonelID);
+            p.PersonelAd = personel.PersonelAd;
+            p.PersonelSoyad = personel.PersonelSoyad;
+            p.PersonelGorsel = personel.PersonelGorsel;
+            p.DepartmanID = personel.DepartmanID;
+            p.Durum = personel.Durum;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
