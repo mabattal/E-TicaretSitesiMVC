45fcd8a8712e6fbfda2b0bd20f769f13e83c7ca1
mabattal <mehmetalibattal@gmail.com>
2023-07-06 22:38:05 +0300

    Satış Index

---
+++ E-TicaretSitesiMVC/Controllers/SatisController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_TicaretSitesiMVC.Models.Siniflar;
+
+namespace E_TicaretSitesiMVC.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var degerler = context.SatisHarekets.ToList();
+            return View(degerler);
+        }
+    }
+}
