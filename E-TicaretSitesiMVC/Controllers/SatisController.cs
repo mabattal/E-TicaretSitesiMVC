d77b5d5b591d5158c152085aac2a0ac03fc736d6
mabattal <mehmetalibattal@gmail.com>
2023-07-07 00:19:27 +0300

    Yeni Satış yapma

--- E-TicaretSitesiMVC/Controllers/SatisController.cs
+++ E-TicaretSitesiMVC/Controllers/SatisController.cs
@@ -16,5 +16,61 @@ namespace E_TicaretSitesiMVC.Controllers
             var degerler = context.SatisHarekets.ToList();
             return View(degerler);
         }
+
+        [HttpGet]
+        public ActionResult SatisEkle()
+        {
+            SatisHareket satisHareket = new SatisHareket();
+
+            //Ürünler dropdown için ürünler listesi
+            List<SelectListItem> list1 = (from x in context.Uruns.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.UrunAd,
+                                             Value = x.UrunID.ToString()
+                                         }).ToList();
+            ViewBag.Urunler = list1;
+
+            //Cariler dropdown için cariler listesi
+            List<SelectListItem> list2 = (from x in context.Caris.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.CariAd + " " + x.CariSoyad,
+                                             Value = x.CariID.ToString()
+                                         }).ToList();
+            ViewBag.Cariler = list2;
+
+            //Personeller dropdown için personeller listesi
+            List<SelectListItem> list3 = (from x in context.Personels.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                              Value = x.PersonelID.ToString()
+                                          }).ToList();
+            ViewBag.Personeller = list3;
+
+            List<SelectListItem> list4 = (from x in context.Uruns.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = x.UrunAd,
+                                              Value = x.UrunID.ToString()
+                                          }).ToList();
+            ViewBag.Urunler = list4;
+
+            return View(satisHareket);
+        }
+
+        [HttpPost]
+        public ActionResult SatisEkle(SatisHareket satisHareket)
+        {
+            //satisHareket'ten gelen UrunID ile ürünü var tipinde bir x değişkenine aldım
+            var x = context.Uruns.Find(satisHareket.UrunID);
+            satisHareket.Fiyat = x.SatisFiyat;                                  
+            satisHareket.ToplamTutar = satisHareket.Fiyat * satisHareket.Adet;
+            satisHareket.Tarih = DateTime.Parse(DateTime.Now.ToString());
+            context.SatisHarekets.Add(satisHareket);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
