fe0c031e3e9b7cab640314f698985e3192390ca2
mabattal <mehmetalibattal@gmail.com>
2023-07-08 13:56:41 +0300

    Fatura Controller, Index

---
+++ E-TicaretSitesiMVC/Controllers/FaturaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_TicaretSitesiMVC.Models.Siniflar;
+
+namespace E_TicaretSitesiMVC.Controllers
+{
+    public class FaturaController : Controller
+    {
+        // GET: Fatura
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var liste = context.Faturas.ToList();
+            return View(liste);
+        }
+    }
+}
