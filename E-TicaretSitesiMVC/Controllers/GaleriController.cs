c8d426f40cf6c734b0fa479dd452dee4b0d3ce2a
mabattal <mehmetalibattal@gmail.com>
2023-07-10 21:36:31 +0300

    Ürün görsellerini çekme

--- E-TicaretSitesiMVC/Controllers/GaleriController.cs
+++ E-TicaretSitesiMVC/Controllers/GaleriController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using E_TicaretSitesiMVC.Models.Siniflar;
 
 namespace E_TicaretSitesiMVC.Controllers
 {
     public class GaleriController : Controller
     {
         // GET: Galeri
+        Context context = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = context.Uruns.ToList();
+            return View(degerler);
         }
     }
 }
