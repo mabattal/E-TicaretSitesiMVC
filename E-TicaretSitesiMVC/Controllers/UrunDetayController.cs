f35a54bea06062420c4ca1d61c6b7cee40964462
mabattal <mehmetalibattal@gmail.com>
2023-07-10 23:36:18 +0300

    Ürün Detay sayfası

---
+++ E-TicaretSitesiMVC/Controllers/UrunDetayController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_TicaretSitesiMVC.Models.Siniflar;
+
+namespace E_TicaretSitesiMVC.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        // GET: UrunDetay
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Uruns = context.Uruns.Where(x => x.UrunID == 1).ToList();
+            cs.Detays = context.Detays.Where(y => y.DetayID == 1).ToList() ;
+            return View(cs);
+        }
+    }
+}
