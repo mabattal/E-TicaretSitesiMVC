282da769c98fa5813a76c2c8e310bbda3a1eaf48
mabattal <mehmetalibattal@gmail.com>
2023-07-12 23:14:58 +0300

    Yapılacak sınıfı code-first migrations eklenmesi

--- E-TicaretSitesiMVC/Models/Siniflar/Context.cs
+++ E-TicaretSitesiMVC/Models/Siniflar/Context.cs
@@ -19,6 +19,7 @@ namespace E_TicaretSitesiMVC.Models.Siniflar
         public DbSet<SatisHareket> SatisHarekets { get; set; }
         public DbSet<Urun> Uruns { get; set; }
         public DbSet<Detay> Detays { get; set; }
+        public DbSet<Yapilacak> Yapilacaks { get;set; }
         
     }
 }
