216bc794a39ada7f3e781c4125230b1661190fca
mabattal <mehmetalibattal@gmail.com>
2023-07-13 17:49:17 +0300

    Yapılacakları listeleme, ekleme, silme

--- E-TicaretSitesiMVC/Controllers/YapilacakController.cs
+++ E-TicaretSitesiMVC/Controllers/YapilacakController.cs
@@ -17,7 +17,32 @@ namespace E_TicaretSitesiMVC.Controllers
             ViewBag.d2 = context.Uruns.Where(x => x.Sil == false).Count().ToString();
             ViewBag.d3 = context.Kategoris.Count().ToString();
             ViewBag.d4 = (from x in context.Caris select x.CariSehir).Distinct().Count().ToString();
-            return View();
+
+            var yapilacaklar = context.Yapilacaks.Where(x => x.Durum == false).ToList();
+            return View(yapilacaklar);
+        }
+
+        public ActionResult YapilacakEkle()
+        {
+            Yapilacak yapilacak = new Yapilacak();
+            return View(yapilacak);
+        }
+
+        [HttpPost]
+        public ActionResult YapilacakEkle(Yapilacak yapilacak)
+        {
+            yapilacak.Tarih = DateTime.Now;
+            context.Yapilacaks.Add(yapilacak);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult YapilacakSil(int id)
+        {
+            var yapilacak = context.Yapilacaks.Find(id);
+            yapilacak.Durum = true;
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
