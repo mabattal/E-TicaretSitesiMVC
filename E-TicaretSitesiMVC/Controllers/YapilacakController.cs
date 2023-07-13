6d49a6c1d528af8aaa241508d4c240ff3df732f1
mabattal <mehmetalibattal@gmail.com>
2023-07-13 15:23:06 +0300

    To Do List kartlar düzenlendi

--- E-TicaretSitesiMVC/Controllers/YapilacakController.cs
+++ E-TicaretSitesiMVC/Controllers/YapilacakController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using E_TicaretSitesiMVC.Models.Siniflar;
 
 namespace E_TicaretSitesiMVC.Controllers
 {
     public class YapilacakController : Controller
     {
         // GET: Yapilacak
+        Context context = new Context();
         public ActionResult Index()
         {
+            ViewBag.d1 = context.Caris.Where(x => x.Sil == false).Count().ToString();
+            ViewBag.d2 = context.Uruns.Where(x => x.Sil == false).Count().ToString();
+            ViewBag.d3 = context.Kategoris.Count().ToString();
+            ViewBag.d4 = (from x in context.Caris select x.CariSehir).Distinct().Count().ToString();
             return View();
         }
     }
