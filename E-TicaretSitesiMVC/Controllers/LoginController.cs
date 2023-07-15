5e6afae44655ec1308596580e7290ecb48396b13
mabattal <mehmetalibattal@gmail.com>
2023-07-15 16:46:43 +0300

    Yeni Cari kaydı

--- E-TicaretSitesiMVC/Controllers/LoginController.cs
+++ E-TicaretSitesiMVC/Controllers/LoginController.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using E_TicaretSitesiMVC.Models.Siniflar;
 
 namespace E_TicaretSitesiMVC.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        Context context = new Context();
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public PartialViewResult KayitOlPartial()
         {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult KayitOlPartial(Cari cari)
+        {
+            cari.Durum = false;
+            cari.Sil = false;
+            context.Caris.Add(cari);
+            context.SaveChanges();
             return PartialView();
         }
     }
