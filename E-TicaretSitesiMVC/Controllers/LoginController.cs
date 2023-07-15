8981ebe8cca34a1b7fbd12c59ba5b4cc245c6691
mabattal <mehmetalibattal@gmail.com>
2023-07-15 20:28:42 +0300

    Login İşlemi partialView'den vazgeçildi

--- E-TicaretSitesiMVC/Controllers/LoginController.cs
+++ E-TicaretSitesiMVC/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using E_TicaretSitesiMVC.Models.Siniflar;
 
 namespace E_TicaretSitesiMVC.Controllers
@@ -17,19 +18,41 @@ namespace E_TicaretSitesiMVC.Controllers
         }
 
         [HttpGet]
-        public PartialViewResult KayitOlPartial()
+        public ActionResult CariKayit()
         {
-            return PartialView();
+            return View();
         }
 
         [HttpPost]
-        public PartialViewResult KayitOlPartial(Cari cari)
+        public ActionResult CariKayit(Cari cari)
         {
             cari.Durum = false;
             cari.Sil = false;
             context.Caris.Add(cari);
             context.SaveChanges();
-            return PartialView();
+            return RedirectToAction("Index", "Login");
+        }
+
+        [HttpGet]
+        public ActionResult CariGiris()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariGiris(Cari cari)
+        {
+            var bilgiler = context.Caris.FirstOrDefault(x => x.CariMail == cari.CariMail && x.Sifre == cari.Sifre && x.Durum == true && x.Sil == false);
+            if(bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CariMail"] = bilgiler.CariMail.ToString();
+                return RedirectToAction("Index", "CariPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }            
         }
     }
 }
