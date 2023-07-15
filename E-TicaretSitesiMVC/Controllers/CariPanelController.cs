2a6d6ca400ffd76ce2c211b68b3bcc11595d5000
mabattal <mehmetalibattal@gmail.com>
2023-07-15 21:07:33 +0300

    Session ile Mail taşıma

--- E-TicaretSitesiMVC/Controllers/CariPanelController.cs
+++ E-TicaretSitesiMVC/Controllers/CariPanelController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E_TicaretSitesiMVC.Models.Siniflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,15 @@ namespace E_TicaretSitesiMVC.Controllers
     public class CariPanelController : Controller
     {
         // GET: CariPanel
+        Context context = new Context();
+
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var mail = (string)Session["CariMail"];
+            var cari = context.Caris.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.adsoy = cari.CariAd + " " + cari.CariSoyad;
+            return View(cari);
         }
     }
 }
